b87d4383645297e6a08bb4719fefe65a78f9112d
ricohoho <ricohoho@gmail.com>
2020-05-18 11:30:06 +0200

    mise a jour de Sheet Google

--- src/main/java/ApiGoogle/SpreadsheetSnippets.java
+++ src/main/java/ApiGoogle/SpreadsheetSnippets.java
@@ -403,4 +403,25 @@ public class SpreadsheetSnippets {
         // [END sheets_conditional_formatting]
         return result;
     }
+    
+    public int columnLetterToNumber(String inputColumnName) {        
+        int outputColumnNumber = 0;
+
+        if (inputColumnName == null || inputColumnName.length() == 0) {
+            throw new IllegalArgumentException("Input is not valid!");
+        }
+
+        int i = inputColumnName.length() - 1;
+        int t = 0;
+        while (i >= 0) {
+            char curr = inputColumnName.charAt(i);
+            outputColumnNumber = outputColumnNumber + (int) Math.pow(26, t) * (curr - 'A' + 1);
+            t++;
+            i--;
+        }
+        System.out.println("ColumnLetterToNumber : " + inputColumnName + " = " +  outputColumnNumber);
+        return outputColumnNumber;
+    }
+    
+    
 }
